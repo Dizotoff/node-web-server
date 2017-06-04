bef9bae445cdaac084cc02236e5c149e283cf7ee
Dizotoff <izotov.dima@gmail.com>
2017-06-04 15:14:18 +0300

    page added

--- server.js
+++ server.js
@@ -37,11 +37,18 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects page'
+  });
+});
+
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page'
   });
 });
+
 
 // /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
